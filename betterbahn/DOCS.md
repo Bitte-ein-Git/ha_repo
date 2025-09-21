4721bd4ba680c18b35cdb3b77e9d5612e6551944
Elias <eliasbetoulle@gmx.net>
2025-09-21 22:14:13 +0200

    change port

---
+++ betterbahn/DOCS.md
@@ -0,0 +1,1 @@
+<https://lukasweihrauch.notion.site/betterbahn>
--- betterbahn/run.sh
+++ betterbahn/run.sh
@@ -3,4 +3,4 @@
 # log addon start
 bashio::log.info "BetterBahn Addon is starting."
 # log web interface port
-bashio::log.info "The web interface will be available at port 3000 inside the container."
+bashio::log.info "The web interface will be available at port 8169 inside the container."
